4d73347944e49e626a4bea3c4c929e41abab33f5
Dami Showunmi <damishowumni@gmail.com>
2024-04-07 07:42:34 -0400

    Added pointing recognition may not wak as intended

--- LeapSDKPython/examples/gesture_tracker.py
+++ LeapSDKPython/examples/gesture_tracker.py
@@ -1,18 +1,37 @@
 import leap
 import time
 
-#Listens for if the user's thumb and index fingers are pinched together
+#Listens for if one of the user's thumb and index fingers are pinched together
 class PinchListener(leap.Listener):
     def on_tracking_event(self, event):
         if event.tracking_frame_id % 5 == 0:
             activeHands = event.hands
             for hand in activeHands:
-                print(hand.pinch_strength ==1)
+                print(hand.pinch_strength ==1)      #pinch_strength equals 1 if fingers are fully pinched
+                
+#Listens for if the user has only index finger extended, not caring about the thumb
+class IndexListener(leap.Listener):
+    def on_tracking_event(self, event):
+        if event.tracking_frame_id % 5 == 0:
+            THUMB = 0
+            INDEX = 1
+            activeHands = event.hands
+            for hand in activeHands:
+                pointing = True                             # make a false value if
+                for finger in hand.digits:                  # only one hand is pointing: defined by if
+                    if finger.finger_id == INDEX:
+                        pointing &= finger.is_extended      # index finger is extended and
+                    elif finger.finger_id != THUMB:
+                        pointing &= not finger.is_extended  # all others (except thumb) are not
+                print(pointing)
 
 def main():
     my_listener = PinchListener()
 
     connection = leap.Connection()
+    connection.add_listener(my_listener)
+
+    my_listener = IndexListener()
     connection.add_listener(my_listener)
 
     with connection.open():
